1974f464b5ec51eb88fff2880542b6b1a35c6457
Bruno Martin <bruno.martin@hacklab.com.br>
2016-07-15 20:12:01 -0300

    add depth = 1 to all serializers for now

--- discussion/serializers.py
+++ discussion/serializers.py
@@ -6,33 +6,39 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
+        depth = 1
 
 
 class ForumSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Forum
+        depth = 1
 
 
 class TopicSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Topic
+        depth = 1
 
 
 class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
+        depth = 1
 
 
 class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
+        depth = 1
 
 
 class TopicNotificationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TopicNotification
+        depth = 1

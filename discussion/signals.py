aec5b65b330b539ce52bb67cf4c5eeae4e3a4ac0
Laury Bueno <laury@hacklab.com.br>
2016-07-22 20:39:00 -0300

    New Comment notifications added

--- discussion/signals.py
+++ discussion/signals.py
@@ -1,9 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from discussion.models import Topic, Comment, CommentHistory, TopicNotification
+from discussion.models import Topic, Comment, CommentHistory, TopicNotification, TopicLike, TopicUse
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model, models
-
 
 @receiver(post_save, sender=Topic)
 def topic_created_or_updated(instance, **kwargs):
@@ -19,14 +18,13 @@ def topic_created_or_updated(instance, **kwargs):
     else:
         users = []
 
-    for person in users:
+    for one_user in users:
         # Create the New Topic notification for appropriate users
         TopicNotification.objects.create(
-            user=person,
+            user=one_user,
             topic=instance,
             action='new_topic',
         )
-
 
     # TODO take real group permissions into account when triggering notifications
 
@@ -36,6 +34,41 @@ def comment_created_or_updated(instance, **kwargs):
 
     # Adjust last_activity_at in Topic
     instance.topic.last_activity_at = instance.updated_at
+
+    # Users that must be notified
+    users = []
+
+    # Notify the topic creator
+    users.append(instance.topic.author)
+
+    # Notify people that already commented ou answered to comments in this topic
+    for comment in Comment.objects.filter(topic=instance.topic):
+        users.append(comment.author)
+
+    # Notify anyone who has reacted to the main topic message
+    for react in TopicLike.objects.filter(topic=instance.topic):
+        users.append(react.user)
+
+    for react in TopicUse.objects.filter(topic=instance.topic):
+        users.append(react.user)
+
+    # Create (or update) the nedded notifications
+    for one_user in users:
+        # Check if the current user already has a pending notification for this topic
+        try:
+            notification = TopicNotification.objects.filter(
+                user=one_user,
+                topic=instance.topic,
+            )
+        except TopicNotification.DoesNotExist:
+            notification = TopicNotification.objects.create(
+                user=one_user,
+                topic=instance.topic,
+            )
+
+        notification.action = 'new_comment'
+        notification.is_read = False
+        notification.is_read = False
 
     coment_revision = CommentHistory()
     coment_revision.create_or_update_revision(instance=instance)

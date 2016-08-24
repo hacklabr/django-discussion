0836fdeac03254ea8136574e3561ad1311220177
Bruno Martin <bruno.martin@hacklab.com.br>
2016-08-24 14:59:59 -0300

    search fields in topic

--- discussion/admin.py
+++ discussion/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from discussion.models import Category, Forum, Topic, Comment, Tag, TopicNotification
 
+
+class TopicAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+
+
 admin.site.register(Category)
 admin.site.register(Forum)
-admin.site.register(Topic)
+admin.site.register(Topic, TopicAdmin)
 admin.site.register(Comment)
 admin.site.register(Tag)
 admin.site.register(TopicNotification)

325819116b79e1c38c93cf637826a846416a428f
Virgílio N Santos <virgilio.santos@gmail.com>
2018-01-15 21:55:22 -0200

    forum form: fix form widget and view

--- discussion/forms.py
+++ discussion/forms.py
@@ -8,7 +8,7 @@ class ForumForm(forms.ModelForm):
         model = Forum
         fields = ['category', 'title', 'text', 'forum_type', 'is_public', 'groups']
         widgets = {
-            'category': forms.Select(**attrs),
+            'category': forms.SelectMultiple(**attrs),
             'title': forms.TextInput(**attrs),
             'textarea': forms.Textarea(**attrs),
             'forum_type': forms.Select(**attrs),

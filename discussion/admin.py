8da84dffe08b3d8c8a629bbbb8b8daed530dec87
Bruno Martin <bruno.martin@hacklab.com.br>
2020-05-02 01:07:34 -0300

    add filter horizontal to groups and category in forum admin

--- discussion/admin.py
+++ discussion/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from discussion.models import Category, Forum, Topic, Comment, Tag, TopicNotification
 
 
+class ForumAdmin(admin.ModelAdmin):
+    filter_horizontal = [
+        'groups',
+        'category',
+    ]
+
 class TopicAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content']
 
 
 admin.site.register(Category)
-admin.site.register(Forum)
+admin.site.register(Forum, ForumAdmin)
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(Comment)
 admin.site.register(Tag)

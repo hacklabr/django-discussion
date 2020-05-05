84ab072186a8697a08b29fe1fce4a9913a1d74d0
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2020-05-05 17:36:19 -0300

    Fix: Solve #107 baquara-iat - Permission to edit forum, topic, comments

--- discussion/permissions.py
+++ discussion/permissions.py
@@ -1,5 +1,20 @@
 from rest_framework import permissions
-from discussion.models import Topic, Comment
+from discussion.models import Topic, Comment, Forum
+
+
+class IsForumAuthor(permissions.BasePermission):
+    """
+    Custom permission to only allow a forum author to edit its own topic
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        elif request.user and request.user.is_superuser:
+            return True
+        elif request.user.is_authenticated and isinstance(obj, Forum) and obj.author == request.user:
+            return True
 
 
 class IsTopicAuthor(permissions.BasePermission):
@@ -14,7 +29,7 @@ class IsTopicAuthor(permissions.BasePermission):
             return True
         elif request.user and request.user.is_superuser:
             return True
-        elif request.user.is_authenticated() and isinstance(obj, Topic) and obj.author == request.user:
+        elif request.user.is_authenticated and isinstance(obj, Topic) and obj.author == request.user:
             return True
 
 
@@ -30,5 +45,5 @@ class IsCommentAuthor(permissions.BasePermission):
             return True
         elif request.user and request.user.is_superuser:
             return True
-        elif request.user.is_authenticated() and isinstance(obj, Comment) and obj.author == request.user:
+        elif request.user.is_authenticated and isinstance(obj, Comment) and obj.author == request.user:
             return True

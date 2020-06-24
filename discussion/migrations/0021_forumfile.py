c93d1720729472eef44c964bb9567c6ab6ce9319
Bruno Martin <bruno.martin@hacklab.com.br>
2020-06-24 04:20:29 -0300

    fix migrations

--- discussion/migrations/0021_forumfile.py
+++ discussion/migrations/0021_forumfile.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=255, null=True, verbose_name='Name', blank=True)),
                 ('file', models.FileField(upload_to=discussion.models.get_upload_path)),
-                ('forum', models.ForeignKey(related_name='files', blank=True, to='discussion.Forum', null=True)),
+                ('forum', models.ForeignKey(related_name='files', on_delete=models.CASCADE, blank=True, to='discussion.Forum', null=True)),
             ],
         ),
     ]
